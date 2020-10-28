08a511691d95d894a812b02112c791d4f6342e3c
Yutoohata <yutoohata@outlook.jp>
2020-10-28 23:06:32 +0900

    section2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -2,14 +2,27 @@
 function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  // noStroke();
+   noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
+    if(i % 2 == 0){
+      fill(blue);
+    }
+    else if(i % 2 == 1){
+      fill(255);
+    }
+    else{
+      noFill();
+    }
+
+
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
     rect(0, i * d, width, (i + 1) * d);
+
+
   }
 
   fill(blue);
@@ -17,5 +30,9 @@ function setup() {
   rect(0, 0, size, size);
 
   fill(255);
+  rect(0, d*2, size, d)
+  rect(d*2, 0, d, size)
+
+
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }
