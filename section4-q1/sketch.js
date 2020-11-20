45b37734492ecdb921a05e3d37ba7d31b211c25a
Yutoohata <yutoohata@outlook.jp>
2020-11-20 22:50:01 +0900

    section4-q2

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -11,6 +11,7 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
+
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  ellipse(width / 2, height / 2, count);
 }
